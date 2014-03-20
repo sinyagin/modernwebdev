7b39027081962569957d459f5dbb7d779770d04a
Andrey Sinyagin <andrey.sinyagin@gmail.com>
2014-03-20 12:16:12 +0200

    add simple handler for button button type=«submit» -> «button»  for not reloading page

---
+++ unit1/assets/js/process.js
@@ -0,0 +1,14 @@
+/**
+ * Created by andrey on 19.03.14.
+ */
+
+/**
+ * Search handler
+ * @param {Event} e
+ */
+function find(e) {
+    console.log('find');
+    var container = document.querySelector('div.container');
+    container.hide();
+    console.log(container);
+}
