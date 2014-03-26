5d833ae02c2cf83a609dda8136ba3dba580db1a0
Andrey Sinyagin <andrey.sinyagin@gmail.com>
2014-03-26 21:12:04 +0200

    small test angular + ng-repeat

--- unit2/scripts/app.js
+++ unit2/scripts/app.js
@@ -1,2 +1,17 @@
 // Use this file to *create* an AngularJS module for your app and configure $routeProvider.
+/// <reference path="../lib/DefinitelyTyped/angularjs/angular.d.ts" />
+var MainController = (function () {
+    function MainController($scope) {
+        $scope.awesomeThings = [
+            'Html1',
+            'Angular',
+            'Karma',
+            'Test12'
+        ];
+    }
+    MainController.$inject = ['$scope'];
+    return MainController;
+})();
+
+angular.module('auction', []).controller('MainCtrl', MainController);
 //# sourceMappingURL=app.js.map
--- unit2/scripts/app.ts
+++ unit2/scripts/app.ts
@@ -1,1 +1,18 @@
 // Use this file to *create* an AngularJS module for your app and configure $routeProvider.
+/// <reference path="../lib/DefinitelyTyped/angularjs/angular.d.ts" />
+
+class MainController {
+
+    static $inject = ['$scope'];
+
+    constructor($scope) {
+        $scope.awesomeThings = [
+            'Html1',
+            'Angular',
+            'Karma',
+            'Test12'
+        ];
+    }
+}
+
+angular.module('auction', []).controller('MainCtrl', MainController);
