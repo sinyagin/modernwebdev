0e96fb5aae7f1b2267b3609a86d9c0ea887048a3
Andrey Sinyagin <andrey.sinyagin@gmail.com>
2014-04-02 10:04:19 +0300

    Homework3 initial commit

---
+++ unit3/app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular
+  .module('unit3App', [
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
